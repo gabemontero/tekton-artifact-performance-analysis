2e13a7563cc379bfa35893151407c15a213b8418
gabemontero <gmontero@redhat.com>
2023-08-01 18:13:44 -0400

    refactor /consolidation

--- main.go
+++ main.go
@@ -31,6 +31,195 @@ func main() {
 	}
 }
 
+var prStartTimes = map[string]time.Time{}
+var prEndTimes = map[string]time.Time{}
+var trStartTimes = map[string]time.Time{}
+var trEndTimes = map[string]time.Time{}
+var podStartTimes = map[string]time.Time{}
+var podEndTimes = map[string]time.Time{}
+
+var prToDuration = map[string]float64{}
+var prDurations = []float64{}
+var prDurationsMap = map[float64]struct{}{}
+var podToDuration = map[string]float64{}
+var podDurations = []float64{}
+var podDurationsMap = map[float64]struct{}{}
+var trToDuration = map[string]float64{}
+var trDurations = []float64{}
+var trDurationsMap = map[float64]struct{}{}
+
+func ignorePipelineRun(pr *v1beta1.PipelineRun, prFilter string) bool {
+	prKey := fmt.Sprintf("%s:%s", pr.Namespace, pr.Name)
+	if len(prFilter) > 0 && prKey != prFilter {
+		return true
+	}
+	if !pr.HasStarted() {
+		return true
+	}
+	if !pr.IsDone() {
+		return true
+	}
+	return false
+}
+
+func ignoreTaskRun(tr *v1beta1.TaskRun, prFilter string) bool {
+	if !tr.HasStarted() {
+		return true
+	}
+	if !tr.IsDone() {
+		return true
+	}
+	trKey := fmt.Sprintf("%s:%s", tr.Namespace, tr.Name)
+	if len(prFilter) > 0 && !strings.HasPrefix(trKey, prFilter) {
+		return true
+	}
+	return false
+}
+
+func prOwns(kidns, kidname, prns, prname string) bool {
+	if kidns != prns {
+		return false
+	}
+	if !strings.HasPrefix(kidname, prname) {
+		return false
+	}
+	return true
+}
+
+func ignorePod(pod *corev1.Pod, prFilter string) bool {
+	if pod.Status.StartTime == nil {
+		return true
+	}
+	if pod.Status.Phase != corev1.PodSucceeded && pod.Status.Phase != corev1.PodFailed {
+		return true
+	}
+	_, ok := pod.Labels["tekton.dev/pipelineRun"]
+	if !ok {
+		return true
+	}
+	podKey := fmt.Sprintf("%s:%s", pod.Namespace, pod.Name)
+	if len(prFilter) > 0 && !strings.HasPrefix(podKey, prFilter) {
+		return true
+	}
+	return false
+}
+
+func processPipelineRun(pr *v1beta1.PipelineRun) time.Duration {
+	duration := pr.Status.CompletionTime.Sub(pr.Status.StartTime.Time)
+	prKey := fmt.Sprintf("%s:%s", pr.Namespace, pr.Name)
+	prToDuration[prKey] = duration.Seconds()
+	_, ok := prDurationsMap[duration.Seconds()]
+	if !ok {
+		prDurations = append(prDurations, duration.Seconds())
+		prDurationsMap[duration.Seconds()] = struct{}{}
+	}
+	prStartTimes[prKey] = pr.Status.StartTime.Time
+	prEndTimes[prKey] = pr.Status.CompletionTime.Time
+	return duration
+}
+
+func processTaskRun(tr *v1beta1.TaskRun) time.Duration {
+	duration := tr.Status.CompletionTime.Sub(tr.Status.StartTime.Time)
+	trKey := fmt.Sprintf("%s:%s", tr.Namespace, tr.Name)
+	trToDuration[trKey] = duration.Seconds()
+	_, ok := trDurationsMap[duration.Seconds()]
+	if !ok {
+		trDurations = append(trDurations, duration.Seconds())
+		trDurationsMap[duration.Seconds()] = struct{}{}
+	}
+	trStartTimes[trKey] = tr.Status.StartTime.Time
+	trEndTimes[trKey] = tr.Status.CompletionTime.Time
+	return duration
+}
+
+func processPod(pod *corev1.Pod) time.Duration {
+	var terimnatedTime time.Time
+	for _, status := range pod.Status.ContainerStatuses {
+		terminated := status.State.Terminated
+		if terminated != nil {
+			if terminated.FinishedAt.Time.After(terimnatedTime) {
+				terimnatedTime = terminated.FinishedAt.Time
+			}
+		}
+	}
+	duration := terimnatedTime.Sub(pod.Status.StartTime.Time)
+	podKey := fmt.Sprintf("%s:%s", pod.Namespace, pod.Name)
+	podToDuration[podKey] = duration.Seconds()
+	podStartTimes[podKey] = pod.Status.StartTime.Time
+	podEndTimes[podKey] = terimnatedTime
+	_, ok := podDurationsMap[duration.Seconds()]
+	if !ok {
+		podDurations = append(podDurations, duration.Seconds())
+		podDurationsMap[duration.Seconds()] = struct{}{}
+	}
+	return duration
+}
+
+func determinePRConcurrency(prKey string) int {
+	return innerConcurrency(prKey, prStartTimes, prEndTimes)
+}
+
+func determineTRConcurrency(trKey string) int {
+	return innerConcurrency(trKey, trStartTimes, trEndTimes)
+}
+
+func determinePodConcurrency(prKey string) int {
+	return innerConcurrency(prKey, podStartTimes, podEndTimes)
+}
+
+func innerConcurrency(key string, starts map[string]time.Time, ends map[string]time.Time) int {
+	st, _ := starts[key]
+	en, _ := ends[key]
+	total := 1
+	for k, start := range starts {
+		if k == key {
+			continue
+		}
+		end, _ := ends[k]
+		if start.Equal(st) && end.Equal(en) {
+			total++
+			continue
+		}
+		if start.Before(en) && end.After(st) {
+			total++
+		}
+	}
+	return total
+}
+
+func parsePipelineRunList(fileName, prFilter string) ([]string, []float64, []int, bool) {
+	buf, err := os.ReadFile(fileName)
+	if err != nil {
+		return []string{fmt.Sprintf("ERROR: problem reading file %s: %s\n", fileName, err.Error())}, nil, nil, false
+	}
+	prList := &v1beta1.PipelineRunList{}
+	err = json.Unmarshal(buf, prList)
+	if err != nil {
+		return []string{fmt.Sprintf("ERROR: file %s not marshalling into a PipelineRun list: %s\n", fileName, err.Error())}, nil, nil, false
+	}
+
+	for _, pr := range prList.Items {
+		if ignorePipelineRun(&pr, prFilter) {
+			continue
+		}
+		processPipelineRun(&pr)
+	}
+	sort.Float64s(prDurations)
+	retS := []string{}
+	retF := []float64{}
+	retI := []int{}
+	for _, duration := range prDurations {
+		for key, value := range prToDuration {
+			if value == duration {
+				retS = append(retS, key)
+				retF = append(retF, value)
+				retI = append(retI, determinePRConcurrency(key))
+			}
+		}
+	}
+	return retS, retF, retI, true
+}
+
 func ParsePipelineRunList() *cobra.Command {
 	parsePRList := &cobra.Command{
 		Use:   "prlist <file location> [<options>]",
@@ -44,51 +233,54 @@ func ParsePipelineRunList() *cobra.Command {
 				return
 			}
 			fileName := args[0]
-			buf, err := os.ReadFile(fileName)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "ERROR: problem reading file %s: %s\n", fileName, err.Error())
-				return
-			}
-			prList := &v1beta1.PipelineRunList{}
-			err = json.Unmarshal(buf, prList)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "ERROR: file %s not marshalling into a PipelineRun list: %s\n", fileName, err.Error())
-				return
-			}
-
-			prToDuration := map[string]float64{}
-			durations := []float64{}
-			durationsMap := map[float64]struct{}{}
-			for _, pr := range prList.Items {
-				if !pr.HasStarted() {
-					//TODO track not started
-					continue
-				}
-
-				if !pr.IsDone() {
-					// TODO track not completed
-					continue
-				}
-
-				duration := pr.Status.CompletionTime.Sub(pr.Status.StartTime.Time)
-				prToDuration[fmt.Sprintf("%s:%s", pr.Namespace, pr.Name)] = duration.Seconds()
-				_, ok := durationsMap[duration.Seconds()]
-				if !ok {
-					durations = append(durations, duration.Seconds())
-					durationsMap[duration.Seconds()] = struct{}{}
+			retS, retF, retI, ok := parsePipelineRunList(fileName, "")
+			w := os.Stdout
+			if !ok {
+				w = os.Stderr
+				for _, str := range retS {
+					fmt.Fprintf(w, str)
 				}
 			}
-			sort.Float64s(durations)
-			for _, duration := range durations {
-				for key, value := range prToDuration {
-					if value == duration {
-						fmt.Fprintf(os.Stdout, "PipelineRun %s\t\ttook %v seconds\n", key, value)
-					}
-				}
+			for i, key := range retS {
+				fmt.Fprintf(w, "PipelineRun %s\t\ttook %v seconds with concurrency at %d\n", key, retF[i], retI[i])
 			}
 		},
 	}
 	return parsePRList
+}
+
+func parsePodList(fileName, prFilter string) ([]string, []float64, []int, bool) {
+	buf, err := os.ReadFile(fileName)
+	if err != nil {
+		return []string{fmt.Sprintf("ERROR: problem reading file %s: %s\n", fileName, err.Error())}, nil, nil, false
+	}
+	podList := &corev1.PodList{}
+	err = json.Unmarshal(buf, podList)
+	if err != nil {
+		return []string{fmt.Sprintf("ERROR: file %s not marshalling into a Pod list: %s\n", fileName, err.Error())}, nil, nil, false
+	}
+
+	for _, pod := range podList.Items {
+		if ignorePod(&pod, prFilter) {
+			continue
+		}
+
+		processPod(&pod)
+	}
+	sort.Float64s(podDurations)
+	retS := []string{}
+	retF := []float64{}
+	retI := []int{}
+	for _, duration := range podDurations {
+		for key, value := range podToDuration {
+			if value == duration {
+				retS = append(retS, key)
+				retF = append(retF, value)
+				retI = append(retI, determinePodConcurrency(key))
+			}
+		}
+	}
+	return retS, retF, retI, true
 }
 
 func ParsePodList() *cobra.Command {
@@ -104,59 +296,55 @@ func ParsePodList() *cobra.Command {
 				return
 			}
 			fileName := args[0]
-			buf, err := os.ReadFile(fileName)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "ERROR: problem reading file %s: %s\n", fileName, err.Error())
+			retS, retF, retI, ok := parsePodList(fileName, "")
+			w := os.Stdout
+			if !ok {
+				w = os.Stderr
+				for _, str := range retS {
+					fmt.Fprintf(w, str)
+				}
 				return
 			}
-			podList := &corev1.PodList{}
-			err = json.Unmarshal(buf, podList)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "ERROR: file %s not marshalling into a Pod list: %s\n", fileName, err.Error())
-				return
-			}
-
-			podToDuration := map[string]float64{}
-			durations := []float64{}
-			durationsMap := map[float64]struct{}{}
-			for _, pod := range podList.Items {
-				if pod.Status.StartTime == nil {
-					//TODO track not started
-					continue
-				}
-
-				if pod.Status.Phase != corev1.PodSucceeded && pod.Status.Phase != corev1.PodFailed {
-					//TODO track still in progress
-					continue
-				}
-
-				var duration time.Duration
-
-				for _, status := range pod.Status.ContainerStatuses {
-					terminated := status.State.Terminated
-					if terminated != nil {
-						duration = terminated.FinishedAt.Sub(pod.Status.StartTime.Time)
-					}
-				}
-
-				podToDuration[fmt.Sprintf("%s:%s", pod.Namespace, pod.Name)] = duration.Seconds()
-				_, ok := durationsMap[duration.Seconds()]
-				if !ok {
-					durations = append(durations, duration.Seconds())
-					durationsMap[duration.Seconds()] = struct{}{}
-				}
-			}
-			sort.Float64s(durations)
-			for _, duration := range durations {
-				for key, value := range podToDuration {
-					if value == duration {
-						fmt.Fprintf(os.Stdout, "Pod %s\t\ttook %v seconds\n", key, value)
-					}
-				}
+			for i, key := range retS {
+				fmt.Fprintf(w, "Pod %s\t\ttook %v seconds concurrency %d\n", key, retF[i], retI[i])
 			}
 		},
 	}
 	return parseTRList
+}
+
+func parseTaskRunList(fileName, prFilter string) ([]string, []float64, []int, bool) {
+	buf, err := os.ReadFile(fileName)
+	if err != nil {
+		return []string{fmt.Sprintf("ERROR: problem reading file %s: %s\n", fileName, err.Error())}, nil, nil, false
+	}
+	trList := &v1beta1.TaskRunList{}
+	err = json.Unmarshal(buf, trList)
+	if err != nil {
+		return []string{fmt.Sprintf("ERROR: file %s not marshalling into a TaskRun list: %s\n", fileName, err.Error())}, nil, nil, false
+	}
+
+	for _, tr := range trList.Items {
+		if ignoreTaskRun(&tr, prFilter) {
+			continue
+		}
+
+		processTaskRun(&tr)
+	}
+	sort.Float64s(trDurations)
+	retS := []string{}
+	retF := []float64{}
+	retI := []int{}
+	for _, duration := range trDurations {
+		for key, value := range trToDuration {
+			if value == duration {
+				retS = append(retS, key)
+				retF = append(retF, value)
+				retI = append(retI, determineTRConcurrency(key))
+			}
+		}
+	}
+	return retS, retF, retI, true
 }
 
 func ParseTaskRunList() *cobra.Command {
@@ -172,47 +360,17 @@ func ParseTaskRunList() *cobra.Command {
 				return
 			}
 			fileName := args[0]
-			buf, err := os.ReadFile(fileName)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "ERROR: problem reading file %s: %s\n", fileName, err.Error())
+			retS, retF, retI, ok := parseTaskRunList(fileName, "")
+			w := os.Stdout
+			if !ok {
+				w = os.Stderr
+				for _, str := range retS {
+					fmt.Fprintf(w, str)
+				}
 				return
 			}
-			trList := &v1beta1.TaskRunList{}
-			err = json.Unmarshal(buf, trList)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "ERROR: file %s not marshalling into a TaskRun list: %s\n", fileName, err.Error())
-				return
-			}
-
-			trToDuration := map[string]float64{}
-			durations := []float64{}
-			durationsMap := map[float64]struct{}{}
-			for _, tr := range trList.Items {
-				if !tr.HasStarted() {
-					//TODO track not startedss
-					continue
-				}
-
-				if !tr.IsDone() {
-					// TODO track not completed
-					continue
-				}
-
-				duration := tr.Status.CompletionTime.Sub(tr.Status.StartTime.Time)
-				trToDuration[fmt.Sprintf("%s:%s", tr.Namespace, tr.Name)] = duration.Seconds()
-				_, ok := durationsMap[duration.Seconds()]
-				if !ok {
-					durations = append(durations, duration.Seconds())
-					durationsMap[duration.Seconds()] = struct{}{}
-				}
-			}
-			sort.Float64s(durations)
-			for _, duration := range durations {
-				for key, value := range trToDuration {
-					if value == duration {
-						fmt.Fprintf(os.Stdout, "TaskRun %s\t\ttook %v seconds\n", key, value)
-					}
-				}
+			for i, key := range retS {
+				fmt.Fprintf(w, "TaskRun %s\t\ttook %v seconds concurrency %d\n", key, retF[i], retI[i])
 			}
 		},
 	}
@@ -232,129 +390,71 @@ func ParseAllThreeLists() *cobra.Command {
 				return
 			}
 			prFileName := args[0]
-			prBuf, perr := os.ReadFile(prFileName)
-			if perr != nil {
-
-			}
 			trFileName := args[1]
-			trBuf, terr := os.ReadFile(trFileName)
-			if terr != nil {
-
-			}
 			podFileName := args[2]
-			podBuf, pderr := os.ReadFile(podFileName)
-			if pderr != nil {
 
-			}
-
-			prList := &v1beta1.PipelineRunList{}
-			perr = json.Unmarshal(prBuf, prList)
-			if perr != nil {
-
-			}
-			trList := &v1beta1.TaskRunList{}
-			terr = json.Unmarshal(trBuf, trList)
-			if terr != nil {
-
-			}
-			podList := &corev1.PodList{}
-			pderr = json.Unmarshal(podBuf, podList)
-			if pderr != nil {
-
-			}
-
-			prToDuration := map[string]float64{}
-			prToTaskDurations := map[string]float64{}
-			prToPodDurations := map[string]float64{}
-			prDurations := []float64{}
-			prDurationsMap := map[float64]struct{}{}
-			for _, pr := range prList.Items {
-				if !pr.HasStarted() {
-					//TODO track not started
-					continue
+			retS1, retF1, retI1, ok1 := parsePipelineRunList(prFileName, "")
+			if !ok1 {
+				for _, s := range retS1 {
+					fmt.Fprintf(os.Stderr, s)
 				}
-
-				if !pr.IsDone() {
-					// TODO track not completed
-					continue
+				return
+			}
+			retS2, retF2, retI2, ok2 := parseTaskRunList(trFileName, "")
+			if !ok2 {
+				for _, s := range retS2 {
+					fmt.Fprintf(os.Stderr, s)
 				}
-
-				prKey := fmt.Sprintf("%s:%s", pr.Namespace, pr.Name)
-				duration := pr.Status.CompletionTime.Sub(pr.Status.StartTime.Time)
-				prToDuration[prKey] = duration.Seconds()
-				_, ok := prDurationsMap[duration.Seconds()]
-				if !ok {
-					prDurations = append(prDurations, duration.Seconds())
-					prDurationsMap[duration.Seconds()] = struct{}{}
+				return
+			}
+			retS3, retF3, retI3, ok3 := parsePodList(podFileName, "")
+			if !ok3 {
+				for _, s := range retS3 {
+					fmt.Fprintf(os.Stderr, s)
 				}
-				for _, tr := range trList.Items {
-					if tr.Namespace != pr.Namespace {
+				return
+			}
+
+			for i, prkey := range retS1 {
+				prDuration := retF1[i]
+				prConcurency := retI1[i]
+
+				totalTRDuration := float64(0)
+				maxTRConcurrency := 0
+				for ii, trKey := range retS2 {
+					if !strings.HasPrefix(trKey, prkey) {
 						continue
 					}
-					if !strings.HasPrefix(tr.Name, pr.Name) {
-						continue
-					}
-					if !tr.HasStarted() {
-						//TODO track not startedss
-						continue
-					}
-
-					if !tr.IsDone() {
-						// TODO track not completed
-						continue
-					}
-
-					duration = tr.Status.CompletionTime.Sub(tr.Status.StartTime.Time)
-					taskDurations := float64(0)
-					taskDurations, ok = prToTaskDurations[prKey]
-					if !ok {
-						taskDurations = duration.Seconds()
-					} else {
-						taskDurations = taskDurations + duration.Seconds()
-					}
-					prToTaskDurations[prKey] = taskDurations
-					for _, pod := range podList.Items {
-						if pod.Namespace != tr.Namespace {
-							continue
-						}
-						if !strings.HasPrefix(pod.Name, tr.Name) {
-							continue
-						}
-						if pod.Status.StartTime == nil {
-							//TODO track not started
-							continue
-						}
-
-						if pod.Status.Phase != corev1.PodSucceeded && pod.Status.Phase != corev1.PodFailed {
-							//TODO track still in progress
-							continue
-						}
-						for _, status := range pod.Status.ContainerStatuses {
-							terminated := status.State.Terminated
-							if terminated != nil {
-								duration = terminated.FinishedAt.Sub(pod.Status.StartTime.Time)
-							}
-						}
-						podDurations := float64(0)
-						podDurations, ok = prToPodDurations[prKey]
-						if !ok {
-							podDurations = duration.Seconds()
-						} else {
-							podDurations = podDurations + duration.Seconds()
-						}
-						prToPodDurations[prKey] = podDurations
+					totalTRDuration = totalTRDuration + retF2[ii]
+					if retI2[ii] > maxTRConcurrency {
+						maxTRConcurrency = retI2[ii]
 					}
 				}
-			}
-			sort.Float64s(prDurations)
-			for _, duration := range prDurations {
-				for key, value := range prToDuration {
-					if value == duration {
-						trDurations, _ := prToTaskDurations[key]
-						podDurations, _ := prToPodDurations[key]
-						fmt.Fprintf(os.Stdout, "PipelineRun %s\t\ttook %v seconds with taskruns %v delta %v percent %f and pods %v delta %v percent %f\n", key, value, trDurations, value-trDurations, trDurations/value, podDurations, value-podDurations, podDurations/value)
+				totalPodDuration := float64(0)
+				maxPodConcurrency := 0
+				for iii, podKey := range retS3 {
+					if !strings.HasPrefix(podKey, prkey) {
+						continue
+					}
+					totalPodDuration = totalPodDuration + retF3[iii]
+					maxPodConcurrency = maxPodConcurrency + retI3[iii]
+					if retI3[iii] > maxPodConcurrency {
+						maxPodConcurrency = retI3[iii]
 					}
 				}
+
+				fmt.Fprintf(os.Stdout, "PipelineRun %s\t\t took %v seconds with pr concurrency %d with taskruns %v seconds delta %v percent %f taskrun max concurrency %d pods %v seconds delta %v percent %f pod max concurrency %d\n",
+					prkey,
+					prDuration,
+					prConcurency,
+					totalTRDuration,
+					prDuration-totalTRDuration,
+					totalTRDuration/prDuration,
+					maxTRConcurrency,
+					totalPodDuration,
+					prDuration-totalPodDuration,
+					totalPodDuration/prDuration,
+					maxPodConcurrency)
 			}
 
 		},
